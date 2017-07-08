6e2d5cfc939d2c2862567b98ff63d7ddbcc0c864
iilei <iilei@users.noreply.github.com>
2017-07-08 20:23:28 +0200

    minor tweak for consistency

--- generate-proxy-config.sh
+++ generate-proxy-config.sh
@@ -20,7 +20,7 @@ for proxy in $proxies; do
     export PROXY_PATH_NAME=$proxy
     export PROXY_PATH_NAME_NS=$(echo $PROXY_PATH_NAME | sed "s/^${PROXY_PATH_PREFIX}${DELIM}//g")
     export PROXY_ENDPOINT_NAME="${PROXY_ENDPOINT_PREFIX}${DELIM}${PROXY_PATH_NAME_NS}"
-    export PROXY_PATH=${!proxy}
+    export PROXY_PATH=${!PROXY_PATH_NAME}
     export PROXY_ENDPOINT=${!PROXY_ENDPOINT_NAME}
 
     envsubst < ./proxy.conf.tpl >> "$OUTFILE"

9cc42ea84e71305d3a72300b38e4161bf460fddb
Jochen Preusche <iilei@users.noreply.github.com>
2017-07-10 10:37:14 +0200

    Switch to sh

--- generate-proxy-config.sh
+++ generate-proxy-config.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 NAMESPACE=${1:-API}
 OUTFILE=${2:-./proxy.conf}
@@ -14,14 +14,14 @@ touch "$OUTFILE"
 
 echo "Preparing to configure Proxy settings based on environment vars"
 
-proxies=$( compgen -A variable | grep -E "${PROXY_PATH_PREFIX}${DELIM}" )
+proxies=$( awk 'BEGIN{for (i in ENVIRON) {print i}}' | grep -E "${PROXY_PATH_PREFIX}${DELIM}" )
 
 for proxy in $proxies; do
   export PROXY_PATH_NAME=$proxy
   export PROXY_PATH_NAME_NS=$(echo $PROXY_PATH_NAME | sed "s/^${PROXY_PATH_PREFIX}${DELIM}//g")
   export PROXY_ENDPOINT_NAME="${PROXY_ENDPOINT_PREFIX}${DELIM}${PROXY_PATH_NAME_NS}"
-  export PROXY_PATH=${!PROXY_PATH_NAME}
-  export PROXY_ENDPOINT=${!PROXY_ENDPOINT_NAME}
+  eval "export PROXY_PATH=\${$PROXY_PATH_NAME}"
+  eval "export PROXY_ENDPOINT=\${$PROXY_ENDPOINT_NAME}"
 
   envsubst < ./proxy.conf.tpl >> "$OUTFILE"
 done;
